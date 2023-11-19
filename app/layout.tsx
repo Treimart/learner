5e4c8a80b7ff2d05cc68d1d9c35e25073fbc4e22
Kristel Kolkanen <kristel.kolkanen@gmail.com>
2023-11-19 14:50:26 +0200

    style: reposition text to center

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default async function RootLayout({
       <body className="bg-background text-foreground">
         <main className="min-h-screen flex flex-col items-center">
           <Navbar />
-          <Container fixed style={{ marginTop: '10rem' }}>
+          <Container maxWidth="sm" style={{ marginTop: '10rem' }}>
               {children}
           </Container>
         </main>
