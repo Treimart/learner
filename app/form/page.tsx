846323c7cc14be0a5f68aaefbe6357608eae6e68
Treimart <treier@tlu.ee>
2023-11-22 14:52:26 +0200

    feat(supabase): read questions

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,3 +1,17 @@
-export default function Form() {
-  return <h1>Form page</h1>
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+import { cookies } from "next/headers"
+import { Box, Button, TextField } from "@mui/material"
+
+export const dynamic = "force-dynamic"
+
+export default async function ServerComponent() {
+  const supabase = createServerComponentClient({ cookies })
+
+  const { data: question } = await supabase.from("question").select()
+
+  return (
+    <div>
+      <pre>{JSON.stringify(question, null, 2)}</pre>
+    </div>
+  )
 }
