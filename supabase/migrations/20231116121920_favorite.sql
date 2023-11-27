3d038f2e99f338083a2d5fbce11078e475db81ee
smroosileht <stella.roosileht@leanest.ee>
2023-11-27 20:09:15 +0200

    db changes, fav btn

--- supabase/migrations/20231116121920_favorite.sql
+++ supabase/migrations/20231116121920_favorite.sql
@@ -4,3 +4,17 @@ favorite (
 );
 
 alter table favorite enable row level security;
+
+create policy "Favorites are viewable by everyone."
+  on public.favorite for select
+  using ( true );
+
+create policy "Users can insert their own favorites."
+  on public.favorite for insert
+  to authenticated
+  with check (true);
+
+create policy "Users can update their own favorites."
+  on public.favorite for update
+  to authenticated
+  with check (true);
