9e26fb81e36a72414b1e672678467ffd91224490
Kristel Kolkanen <kristel.kolkanen@gmail.com>
2023-11-29 19:08:26 +0200

    add rls policy

--- supabase/migrations/20231116121943_question.sql
+++ supabase/migrations/20231116121943_question.sql
@@ -13,3 +13,8 @@ alter table question enable row level security;
 create policy "Questions are viewable by everyone."
   on public.question for select
 using ( true );
+
+create policy "Users can create a question."
+  on public.question for insert
+  to authenticated
+  with check (true);
