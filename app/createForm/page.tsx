e7427aeaec8ce7c837c70916bb45183ac15d1ca5
KristelKolkanen <105215931+KristelKolkanen@users.noreply.github.com>
2023-11-29 13:06:02 +0200

    Delete app/createForm/page.tsx

--- app/createForm/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function CreateForm() {
-  return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-    <h1>
-      form creation page
-    </h1>
-  </div>
-  )
-}
