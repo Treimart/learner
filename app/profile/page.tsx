05c5b69138aa3b966b17f6b3ef306aa0b389d7de
Treimart <treier@tlu.ee>
2023-11-30 15:03:12 +0200

    fix: rebase

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -36,33 +36,35 @@ export default function Profile() {
 
   const renderUserInfoAndActions = (userInfoArray: any[]) => {
     const userInfoItems = userInfoArray.map((info, index) => (
-      <Box key={index} sx={{ margin: '3vh 0 3vh 0' }}>{info}</Box>
-    ));
+      <Box
+        key={index}
+        sx={{ margin: "3vh 0 3vh 0" }}
+      >
+        {info}
+      </Box>
+    ))
 
-    return (
-      <div>
-        {userInfoItems}
-      </div>
-    );
-  };
+    return <div>{userInfoItems}</div>
+  }
 
   const renderUserHistoryAndFavorites = (userHistoryArray: any[]) => {
     const userHistoryItems = userHistoryArray.map((info, index) => (
-      <Box key={index} sx={{ margin: '3vh 0 3vh 0' }}>{info}</Box>
-    ));
+      <Box
+        key={index}
+        sx={{ margin: "3vh 0 3vh 0" }}
+      >
+        {info}
+      </Box>
+    ))
 
-    return (
-      <div>
-        {userHistoryItems}
-      </div>
-    )
+    return <div>{userHistoryItems}</div>
   }
 
   let userInfoArray: string[] = [" "];
   if (userData && userData.email) {
     userInfoArray = [`Email: ${userData.email}`];
   }
-  const userHistoryArray = ["Ajalugu", "Lemmikud", "Minu küsimustikud"];
+  const userHistoryArray = ["Ajalugu", "Lemmikud", "Minu küsimustikud"]
 
   if (userDataLoaded) {
     return userData.email != undefined ? (
