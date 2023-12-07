d8f420e7d80b25ed40b169728450e3b6cd543a32
Treimart <treier@tlu.ee>
2023-12-07 16:23:30 +0200

    style: relocate user forms

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,22 +1,21 @@
-'use client'
-import AuthButton from '@/components/AuthButton';
-import { Box, Container, Grid, Button } from '@mui/material';
-import { useEffect, useState } from 'react';
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import ShowUserForms from '@/components/ShowUserForms';
-import ShowUserFavorites from '@/components/ShowUserFavorites';
-import ShowUserHistory from '@/components/ShowUserHistory';
+"use client"
+import AuthButton from "@/components/AuthButton"
+import { Box, Container, Grid, Button } from "@mui/material"
+import { useEffect, useState } from "react"
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
+import ShowUserForms from "@/components/ShowUserForms"
+import ShowUserFavorites from "@/components/ShowUserFavorites"
+import ShowUserHistory from "@/components/ShowUserHistory"
 
 export default function Profile() {
-
   const supabase = createClientComponentClient()
   const [userDataLoaded, setUserDataLoaded] = useState(false)
   const [userData, setUserData] = useState<{
-    id: string;
+    id: string
     email: string | undefined
   }>({
     id: "",
-    email: undefined,
+    email: undefined
   })
 
   useEffect(() => {
@@ -24,10 +23,12 @@ export default function Profile() {
       const { data } = await supabase.auth.getSession()
       //console.log(data.session)
       if (data.session != null) {
-        const { data: {user} } = await supabase.auth.getUser()
+        const {
+          data: { user }
+        } = await supabase.auth.getUser()
         const newData = {
           id: user!.id,
-          email: user!.email || undefined,
+          email: user!.email || undefined
         }
         setUserData(newData)
       }
@@ -38,70 +39,82 @@ export default function Profile() {
 
   const renderUserInfoAndActions = (userInfoArray: any[]) => {
     const userInfoItems = userInfoArray.map((info, index) => (
-      <Box key={index} sx={{ margin: '3vh 0 3vh 0' }}>{info}</Box>
-    ));
+      <Box
+        key={index}
+        sx={{ margin: "3vh 0 3vh 0" }}
+      >
+        {info}
+      </Box>
+    ))
 
-    return (
-      <div>
-        {userInfoItems}
-      </div>
-    );
-  };
+    return <div>{userInfoItems}</div>
+  }
 
   const renderUserHistoryAndFavorites = (userHistoryArray: any[]) => {
     const userHistoryItems = userHistoryArray.map((info, index) => (
-      <Box key={index} sx={{ margin: '3vh 0 3vh 0' }}>{info}</Box>
-    ));
+      <Box
+        key={index}
+        sx={{ margin: "3vh 0 3vh 0" }}
+      >
+        {info}
+      </Box>
+    ))
 
-    return (
-      <div>
-        {userHistoryItems}
-      </div>
-    )
+    return <div>{userHistoryItems}</div>
   }
 
   const resetPassword = async () => {
     window.location.href = "/changePassword"
   }
 
-  let userInfoArray: string[] = [" "];
+  let userInfoArray: string[] = [" "]
   if (userData && userData.email) {
-    userInfoArray = [`Email: ${userData.email}`];
+    userInfoArray = [`Email: ${userData.email}`]
   }
-  const userHistoryArray = [<ShowUserHistory/>, <ShowUserFavorites />, <ShowUserForms />];
+  const userHistoryArray = [<ShowUserHistory />, <ShowUserFavorites />]
 
   if (userDataLoaded) {
     return userData.email != undefined ? (
-      <Container sx={{ width: '100%' }}>
+      <Container sx={{ width: "100%" }}>
         PROFIILI ANDMED
-        <Grid container spacing={2}>
-        <Grid item xs={6}>
-          {renderUserInfoAndActions(userInfoArray)}
-          <Box sx={{ margin: '3vh 0 3vh 0' }}>
-            <Button 
-              onClick={() => resetPassword()}
-              variant="contained"
-              color="primary"
-            >
-              Muuda parooli
-            </Button>
-          </Box>
-          <Box sx={{ margin: '3vh 0 3vh 0' }}>
-            <AuthButton userData={userData} />
-          </Box>
+        <Grid
+          container
+          spacing={2}
+        >
+          <Grid
+            item
+            xs={6}
+          >
+            {renderUserInfoAndActions(userInfoArray)}
+            <Box sx={{ margin: "3vh 0 3vh 0" }}>
+              <Button
+                onClick={() => resetPassword()}
+                variant="contained"
+                color="primary"
+              >
+                Muuda parooli
+              </Button>
+            </Box>
+            <Box sx={{ margin: "3vh 0 3vh 0" }}>
+              <AuthButton userData={userData} />
+            </Box>
+          </Grid>
+          <Grid
+            item
+            xs={6}
+          >
+            {renderUserHistoryAndFavorites(userHistoryArray)}
+          </Grid>
+          <ShowUserForms />
         </Grid>
-        <Grid item xs={6}>
-          {renderUserHistoryAndFavorites(userHistoryArray)}
-        </Grid>
-      </Grid>
       </Container>
     ) : (
       <Container
-      sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        width: '100%'
-      }}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          width: "100%"
+        }}
       >
         <AuthButton userData={userData} />
       </Container>
