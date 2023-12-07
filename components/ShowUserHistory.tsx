e7d0a39f4961fc6cae3207fdafe3f81e8b608538
Treimart <treier@tlu.ee>
2023-12-07 18:59:01 +0200

    style: change Typography to h4

--- components/ShowUserHistory.tsx
+++ components/ShowUserHistory.tsx
@@ -1,82 +1,82 @@
-"use client"
+"use client";
 
-import { Box, Button, Typography } from "@mui/material"
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
-import { useRouter } from "next/navigation"
-import { useEffect, useState } from "react"
+import { Box, Button, Typography } from "@mui/material";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 interface Form {
-  id: number,
-  title: string,
-  time: string,
+  id: number;
+  title: string;
+  time: string;
 }
 
 export default function ShowUserHistory() {
-  const supabase = createClientComponentClient()
-  const router = useRouter()
-  const [userID, setUserID] = useState("")
-  const [userIDLoaded, setUserIDLoaded] = useState(false)
+  const supabase = createClientComponentClient();
+  const router = useRouter();
+  const [userID, setUserID] = useState("");
+  const [userIDLoaded, setUserIDLoaded] = useState(false);
 
   useEffect(() => {
     const getUserID = async () => {
-      const { data } = await supabase.auth.getSession()
+      const { data } = await supabase.auth.getSession();
       if (data.session != null) {
-        const id = data.session.user.id
-        setUserID(id)
+        const id = data.session.user.id;
+        setUserID(id);
       } else {
-        setUserID("0")
+        setUserID("0");
       }
-      setUserIDLoaded(true)
-    }
-    getUserID()
-  }, [])
+      setUserIDLoaded(true);
+    };
+    getUserID();
+  }, []);
 
-  const [history, setHistory] = useState<Form[]>([])
+  const [history, setHistory] = useState<Form[]>([]);
 
   const getHistroy = async () => {
     const { data } = await supabase
       .from("history")
-      .select('id, user_id, form_id, lastcompletion, form(id, title)')
-      .eq('user_id', userID)
-      .order('lastcompletion', {ascending: false})
+      .select("id, user_id, form_id, lastcompletion, form(id, title)")
+      .eq("user_id", userID)
+      .order("lastcompletion", { ascending: false });
     if (data) {
-      const mappedForm: Form[] = data.map((item:any) => ({
+      const mappedForm: Form[] = data.map((item: any) => ({
         id: item.form.id,
         title: item.form.title,
-        time: item.lastcompletion
-      }))
-      setHistory(mappedForm)
+        time: item.lastcompletion,
+      }));
+      setHistory(mappedForm);
     }
-  }
+  };
 
   useEffect(() => {
     if (userIDLoaded) {
-      getHistroy()
+      getHistroy();
     }
-  }, [userID])
+  }, [userID]);
 
   return (
     <Box
       sx={{
-        display: 'flex',
-        flexDirection: 'column'
+        display: "flex",
+        flexDirection: "column",
       }}
     >
-      <Typography variant="h3">Sinu ajalugu</Typography>
-      {history.map(form => (
-        <Button 
+      <Typography variant="h4">Sinu ajalugu</Typography>
+      {history.map((form) => (
+        <Button
           key={form.id}
           sx={{
-            justifyContent: 'flex-start',
-            width: 'fit-content'
+            justifyContent: "flex-start",
+            width: "fit-content",
           }}
-          variant="text" 
+          variant="text"
           color="primary"
-          onClick={() => router.push('/form?form_id='+form.id)} 
+          onClick={() => router.push("/form?form_id=" + form.id)}
         >
           {form.title}
         </Button>
       ))}
     </Box>
-  )
+  );
 }
