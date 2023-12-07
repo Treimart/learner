45987d63e5b096da2d83c5b337a1596083951201
Treimart <treier@tlu.ee>
2023-12-07 18:53:24 +0200

    style: title for user favorites is now h3 variant

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,90 +1,78 @@
-"use client"
-import AuthButton from "@/components/AuthButton"
-import { Box, Container, Grid, Button, Typography } from "@mui/material"
-import { useEffect, useState } from "react"
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
-import ShowUserForms from "@/components/ShowUserForms"
-import ShowUserFavorites from "@/components/ShowUserFavorites"
-import ShowUserHistory from "@/components/ShowUserHistory"
+"use client";
+import AuthButton from "@/components/AuthButton";
+import { Box, Container, Grid, Button, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import ShowUserForms from "@/components/ShowUserForms";
+import ShowUserFavorites from "@/components/ShowUserFavorites";
+import ShowUserHistory from "@/components/ShowUserHistory";
 
 export default function Profile() {
-  const supabase = createClientComponentClient()
-  const [userDataLoaded, setUserDataLoaded] = useState(false)
+  const supabase = createClientComponentClient();
+  const [userDataLoaded, setUserDataLoaded] = useState(false);
   const [userData, setUserData] = useState<{
-    id: string
-    email: string | undefined
+    id: string;
+    email: string | undefined;
   }>({
     id: "",
-    email: undefined
-  })
+    email: undefined,
+  });
 
   useEffect(() => {
     const getUserData = async () => {
-      const { data } = await supabase.auth.getSession()
+      const { data } = await supabase.auth.getSession();
       //console.log(data.session)
       if (data.session != null) {
         const {
-          data: { user }
-        } = await supabase.auth.getUser()
+          data: { user },
+        } = await supabase.auth.getUser();
         const newData = {
           id: user!.id,
-          email: user!.email || undefined
-        }
-        setUserData(newData)
+          email: user!.email || undefined,
+        };
+        setUserData(newData);
       }
-      setUserDataLoaded(true)
-    }
-    getUserData()
-  }, [])
+      setUserDataLoaded(true);
+    };
+    getUserData();
+  }, []);
 
   const renderUserInfoAndActions = (userInfoArray: any[]) => {
     const userInfoItems = userInfoArray.map((info, index) => (
-      <Box
-        key={index}
-        sx={{ margin: "3vh 0 3vh 0" }}
-      >
+      <Box key={index} sx={{ margin: "3vh 0 3vh 0" }}>
         {info}
       </Box>
-    ))
+    ));
 
-    return <div>{userInfoItems}</div>
-  }
+    return <div>{userInfoItems}</div>;
+  };
 
   const renderUserHistoryAndFavorites = (userHistoryArray: any[]) => {
     const userHistoryItems = userHistoryArray.map((info, index) => (
-      <Box
-        key={index}
-        sx={{ margin: "3vh 0 3vh 0" }}
-      >
+      <Box key={index} sx={{ margin: "3vh 0 3vh 0" }}>
         {info}
       </Box>
-    ))
+    ));
 
-    return <div>{userHistoryItems}</div>
-  }
+    return <div>{userHistoryItems}</div>;
+  };
 
   const resetPassword = async () => {
-    window.location.href = "/changePassword"
-  }
+    window.location.href = "/changePassword";
+  };
 
-  let userInfoArray: string[] = [" "]
+  let userInfoArray: string[] = [" "];
   if (userData && userData.email) {
-    userInfoArray = [`Email: ${userData.email}`]
+    userInfoArray = [`Email: ${userData.email}`];
   }
-  const userHistoryArray = [<ShowUserHistory />]
+  const userHistoryArray = [<ShowUserHistory />];
 
   if (userDataLoaded) {
     return userData.email != undefined ? (
       <Container sx={{ width: "100%" }}>
         PROFIILI ANDMED
-        <Grid
-          container
-          spacing={2}
-        >
-          <Grid
-            item
-            xs={6}
-          >
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
             {renderUserInfoAndActions(userInfoArray)}
             <Box sx={{ margin: "3vh 0 3vh 0" }}>
               <Button
@@ -99,23 +87,14 @@ export default function Profile() {
               <AuthButton userData={userData} />
             </Box>
           </Grid>
-          <Grid
-            item
-            xs={6}
-          >
+          <Grid item xs={6}>
             {renderUserHistoryAndFavorites(userHistoryArray)}
           </Grid>
-          <Typography
-            variant="h4"
-            sx={{ margin: "25px 0" }}
-          >
+          <Typography variant="h3" sx={{ margin: "25px 0" }}>
             Sinu lemmikud
           </Typography>
           <ShowUserFavorites />
-          <Typography
-            variant="h3"
-            sx={{ margin: "25px 0" }}
-          >
+          <Typography variant="h3" sx={{ margin: "25px 0" }}>
             Sinu küsimustikud
           </Typography>
           <ShowUserForms />
@@ -126,11 +105,11 @@ export default function Profile() {
         sx={{
           display: "flex",
           justifyContent: "center",
-          width: "100%"
+          width: "100%",
         }}
       >
         <AuthButton userData={userData} />
       </Container>
-    )
+    );
   }
 }
