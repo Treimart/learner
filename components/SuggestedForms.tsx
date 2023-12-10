f4b997f78efba354a880a477b05bd9d450fddf1c
Treimart <treier@tlu.ee>
2023-12-10 14:33:40 +0200

    feat: show suggested forms

---
+++ components/SuggestedForms.tsx
@@ -0,0 +1,184 @@
+"use client";
+
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import {
+  Box,
+  Grid,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import { format } from "date-fns";
+
+interface Form {
+  id: number;
+  title: string;
+  description: string;
+}
+
+export default function ShowUserForms() {
+  const supabase = createClientComponentClient();
+  const router = useRouter();
+
+  const [userID, setUserID] = useState("");
+  const [userIDLoaded, setUserIDLoaded] = useState(false);
+
+  useEffect(() => {
+    const getUserID = async () => {
+      const { data } = await supabase.auth.getSession();
+      //console.log(data.session)
+      if (data.session != null) {
+        const id = data.session.user.id;
+        setUserID(id);
+      } else {
+        console.log("123");
+        setUserID("0");
+      }
+      setUserIDLoaded(true);
+    };
+    getUserID();
+  }, []);
+
+  const [forms, setForms] = useState<Form[]>([]);
+
+  const getForms = async () => {
+    const currentTimestamp = format(new Date(), "yyyy-MM-dd HH:mm:ss");
+    let data;
+    if (userID === "0") {
+      ({ data } = await supabase
+        .from("form")
+        .select()
+        .or(`deleted.is.null,deleted.gt.${currentTimestamp}`)
+        .order("created", { ascending: false }));
+    } else {
+      ({ data } = await supabase
+        .from("form")
+        .select()
+        .or(`user_id.neq.${userID},user_id.is.null`)
+        .or(`deleted.is.null,deleted.gt.${currentTimestamp}`)
+        .order("created", { ascending: false })
+        .limit(3));
+    }
+    if (data) {
+      setForms(data);
+    }
+  };
+
+  //console.log(userID)
+
+  useEffect(() => {
+    if (userIDLoaded) {
+      getForms();
+      //console.log(userIDLoaded, userID)
+    }
+  }, [userID]);
+
+  const handleClick = (id: number) => {
+    router.push(`../form?form_id=${id}`);
+  };
+
+  const handleStatusChange = async (
+    id: number,
+    event: SelectChangeEvent<number>
+  ) => {
+    const newStatus = event.target.value;
+    const currentTimestamp = format(new Date(), "yyyy-MM-dd HH:mm:ss");
+    console.log("Changing status for form id", id, "to", newStatus);
+    const { data, error } = await supabase
+      .from("form")
+      .update({ status: newStatus, updated: currentTimestamp })
+      .match({ id: id })
+      .select();
+    if (error) {
+      console.error("Error updating status:", error);
+    } else {
+      console.log("Updated form:", data);
+    }
+    getForms();
+  };
+
+  const deleteForm = async (id: number) => {
+    const currentTimestamp = format(new Date(), "yyyy-MM-dd HH:mm:ss");
+    const { data, error } = await supabase
+      .from("form")
+      .update({ deleted: currentTimestamp })
+      .match({ id: id })
+      .select();
+    if (error) {
+      console.error("Error updating delete:", error);
+    } else {
+      console.log("Deleted form:", data);
+      getForms();
+    }
+  };
+
+  return (
+    <>
+      <Typography variant="h4" sx={{ margin: "25px 0" }}>
+        Soovitatud küsimustikud
+      </Typography>
+      <Grid
+        container
+        sx={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(350px, 1fr))",
+          gap: "16px",
+          justifyContent: "space-between",
+          alignItems: "baseline",
+        }}
+      >
+        {forms.map((form) => (
+          <Grid
+            item
+            key={form.id}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              border: 1,
+              borderColor: "primary.main",
+              boxShadow: 1,
+              borderRadius: 2,
+              p: 2,
+              m: 1,
+              width: 350,
+              height: 125,
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <Button
+                key={form.id}
+                sx={{
+                  justifyContent: "flex-start",
+                  width: "fit-content",
+                  mr: 2,
+                }}
+                variant="outlined"
+                color="primary"
+                onClick={() => handleClick(form.id)}
+              >
+                {form.title}
+              </Button>
+            </Box>
+            <Typography sx={{ minHeight: "60px", margin: "0.65em 0" }}>
+              {form.description}
+            </Typography>
+          </Grid>
+        ))}
+      </Grid>
+    </>
+  );
+}
