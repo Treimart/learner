eb644f6507a14473f4e6253ed0d5c795cc1cba43
smroosileht <stella.roosileht@leanest.ee>
2023-12-11 16:03:03 +0200

    style: global css

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import MainPageForms from "@/components/MainPageForms"
 export default async function Index() {
 
   return (
-    <div className="flex-1 flex flex-col w-full px-8 justify-center gap-2">
+    <div>
       <InspirationalQuote />
       <MainPageForms />
     </div>
